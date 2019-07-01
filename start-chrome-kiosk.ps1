674423ebfa8a1184a0ad0e1ac4ef1441ce421176
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-07-01 19:29:36 +0200

    Update start-chrome-kiosk.ps1

--- start-chrome-kiosk.ps1
+++ start-chrome-kiosk.ps1
@@ -2,27 +2,46 @@
 # Author: Dan Michael
 # Modified: 2019-07-01
 
-# 1. Blank slate: Ensure Chrome is not running
+# ------------------------------------------------------------------------------------
+# Settings:
+
+# Path to some powerpoint file that can run in the background to prevent lock screen:
+$powerpointFile = "C:\scriptotek\background.pptx"
+
+# The URL for the Kiosk application:
+$kioskUrl = "http://ub-www01.uio.no/propaganda/kommende-disputaser/mn.html"
+
+# ------------------------------------------------------------------------------------
+
+
+# 1. Ensure clean slate where nothing is running already
+Get-Process -Name POWERPNT -ErrorAction SilentlyContinue | Stop-Process
 Get-Process -Name chrome -ErrorAction SilentlyContinue | Stop-Process
 Wait-Process -Name chrome -ErrorAction SilentlyContinue
+Wait-Process -Name PWOERPNT -ErrorAction SilentlyContinue
 
-# 2. Avoid Chrome restore dialog if Chrome did not exit cleanly
+# 2. Start Powerpoint in background to prevent lock screen
+$powerpoint = New-Object -ComObject powerpoint.application
+$powerpoint.visible = [Microsoft.Office.Core.MsoTriState]::msoTrue
+$presentation = $powerpoint.Presentations.open($powerpointFile) 
+$presentation.SlideShowSettings.Run()
+
+# 3. Avoid Chrome restore dialog if Chrome did not exit cleanly
 $chrome_prefs_file = Join-Path ${env:LOCALAPPDATA} "Google\Chrome\User Data\Default\Preferences"
 $prefs = Get-Content $chrome_prefs_file -raw | ConvertFrom-Json
 $prefs.profile.exit_type = "Normal"
 $prefs.profile.exited_cleanly = $true
 $prefs | ConvertTo-Json -depth 32 | set-content $chrome_prefs_file
       
-# 3. Wait for network	  
+# 4. Wait for network	  
 Write-Host "Waiting for network..."
 do {
   $ping = test-connection -comp uio.no -count 1 -Quiet
 } until ($ping)
 
-# 4. Start Chrome
-$url = "http://ub-www01.uio.no/propaganda/kommende-disputaser/mn.html"
+# 5. Start Chrome
 $chrome = Join-Path ${env:ProgramFiles(x86)} "Google\Chrome\Application\chrome.exe"
-$chrome_args = "--kiosk --noerrdialogs --disable-infobars $url"
+$chrome_args = "--kiosk --noerrdialogs --disable-infobars $kioskUrl"
 
 Write-Host "Starting browser"
 Start-Process $chrome $chrome_args
