a64e8cad75210e63845a0efedbc6424e0aee79ca
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-07-01 19:37:51 +0200

    Update start-chrome-kiosk.ps1

--- start-chrome-kiosk.ps1
+++ start-chrome-kiosk.ps1
@@ -15,18 +15,21 @@ $kioskUrl = "http://ub-www01.uio.no/propaganda/kommende-disputaser/mn.html"
 
 
 # 1. Ensure clean slate where nothing is running already
-Get-Process -Name POWERPNT -ErrorAction SilentlyContinue | Stop-Process
-Wait-Process -Name POWERPNT -ErrorAction SilentlyContinue
-Get-Process -Name chrome -ErrorAction SilentlyContinue | Stop-Process
-Wait-Process -Name chrome -ErrorAction SilentlyContinue
+$apps = @("POWERPNT", "chrome")
+foreach ($app in $apps) {
+	Get-Process -Name $app -ErrorAction SilentlyContinue | Stop-Process
+	Wait-Process -Name $app -ErrorAction SilentlyContinue
+}
 
 # 2. Start Powerpoint in background to prevent lock screen
+Write-Host "Starting powerpoint..."
 $powerpoint = New-Object -ComObject powerpoint.application
 $powerpoint.visible = [Microsoft.Office.Core.MsoTriState]::msoTrue
 $presentation = $powerpoint.Presentations.open($powerpointFile) 
 $presentation.SlideShowSettings.Run()
 
 # 3. Avoid Chrome restore dialog if Chrome did not exit cleanly
+Write-Host "Fixing Chrome prefs..."
 $chrome_prefs_file = Join-Path ${env:LOCALAPPDATA} "Google\Chrome\User Data\Default\Preferences"
 $prefs = Get-Content $chrome_prefs_file -raw | ConvertFrom-Json
 $prefs.profile.exit_type = "Normal"
@@ -36,14 +39,13 @@ $prefs | ConvertTo-Json -depth 32 | set-content $chrome_prefs_file
 # 4. Wait for network	  
 Write-Host "Waiting for network..."
 do {
-  $ping = test-connection -comp uio.no -count 1 -Quiet
+	$ping = Test-Connection -ComputerName uio.no -Count 1 -Quiet
 } until ($ping)
 
 # 5. Start Chrome
+Write-Host "Starting browser"
 $chrome = Join-Path ${env:ProgramFiles(x86)} "Google\Chrome\Application\chrome.exe"
 $chrome_args = "--kiosk --noerrdialogs --disable-infobars $kioskUrl"
-
-Write-Host "Starting browser"
 Start-Process $chrome $chrome_args
 
 # Debug:
